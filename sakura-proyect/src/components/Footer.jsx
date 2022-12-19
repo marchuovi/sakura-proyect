ff66d33acfcbf91aa88f2edd6b03c96f0d786678
maricarmen <mchuecooviedo@gmail.com>
2022-12-19 12:44:56 +0100

    update footercss

--- sakura-proyect/src/components/Footer.jsx
+++ sakura-proyect/src/components/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import logofooter from '../assets/logofooter.png'
+import '../styles/footer.css'
 
 function Footer() {
   return (
-    <div>
+    <div className='footer'>
       <img src= {logofooter} alt="" />
       <h6>Copy Right Sakura Tarot | 2022</h6>
     </div>
