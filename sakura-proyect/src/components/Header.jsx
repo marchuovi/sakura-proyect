8b4e2b02e7b965d9411271268b24d9ff8cac31d0
maricarmen <mchuecooviedo@gmail.com>
2022-12-19 11:19:36 +0100

    header.css

--- sakura-proyect/src/components/Header.jsx
+++ sakura-proyect/src/components/Header.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import Logo from '../assets/logo.png';
-import '../styles/header.css';
+import React from "react";
+import Logo from "../assets/logo.png";
+import "../styles/header.css";
 import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className='header'>
+    <div className="header">
       <img src={Logo} alt="Logo Sakura Tarot" />
-      <div>
-        <nav>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/destino">Destino</Link>
-            </li>
-        </nav>
-      </div>
+
+      <nav>
+        <button>
+          <Link to="/">Home</Link>
+        </button>
+        <button>
+          <Link to="/destino">Destino</Link>
+        </button>
+      </nav>
     </div>
-  )
+  );
 }
